83ebd2130f7bb6af4c98ceca4d3b2f11f9bd6008
Bogdan <bolshakov.production@yandex.ru>
2021-06-21 16:34:06 +0300

    Done on Lesson№8.10

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "external_ip_address_app" {
-  value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+  output "external_ips" {
+  value = [for i in yandex_compute_instance.app : i.network_interface.0.nat_ip_address]
+}
+
+output "external_ip_address_balancer" {
+  value = yandex_lb_network_load_balancer.lb.listener.*.external_address_spec
 }
