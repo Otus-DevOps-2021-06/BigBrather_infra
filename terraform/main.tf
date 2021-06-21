717a25389f2e857340c6c2c798b1647ef055ac9a
Bogdan <bolshakov.production@yandex.ru>
2021-06-21 16:18:08 +0300

    Done on Lesson№8.6

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "yandex_compute_instance" "app" {
 
   connection {
     type  = "ssh"
-    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    host  = self.app.network_interface.0.nat_ip_address
     user  = "ubuntu"
     agent = false
     # путь до приватного ключа
